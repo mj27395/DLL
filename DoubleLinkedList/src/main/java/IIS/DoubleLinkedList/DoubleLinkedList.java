68649594adffdd66b7a8d53b0235ef700113fd39
Maria Jose <mjsalasm27@gmail.com>
2015-03-30 13:12:20 +0200

    Created DoubleLinkedList class and constructor

--- DoubleLinkedList/src/main/java/IIS/DoubleLinkedList/DoubleLinkedList.java
+++ DoubleLinkedList/src/main/java/IIS/DoubleLinkedList/DoubleLinkedList.java
@@ -1,7 +1,12 @@
 package IIS.DoubleLinkedList;
 
 public class DoubleLinkedList<E> {
-	DoubleLinkedNode<E> first,last;
+	DoubleLinkedNode<E> firstNode,lastNode;
 	
-	
+	public DoubleLinkedList(DoubleLinkedNode<E> node){
+		node.last=null;
+		node.previous=null;
+		firstNode=node;
+		lastNode=node;
+	}
 }
