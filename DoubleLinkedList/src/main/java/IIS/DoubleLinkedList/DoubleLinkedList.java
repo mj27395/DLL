ca996d44a09c80eb071b0b603084742497887d7a
Maria Jose <mjsalasm27@gmail.com>
2015-03-31 11:21:23 +0200

    Finished all the DoubleLinkedList methods

--- DoubleLinkedList/src/main/java/IIS/DoubleLinkedList/DoubleLinkedList.java
+++ DoubleLinkedList/src/main/java/IIS/DoubleLinkedList/DoubleLinkedList.java
@@ -40,4 +40,72 @@ public class DoubleLinkedList<E> {
 		
 		node.next = nodeToInsert;
 	}
+	public void insertBeginning(DoubleLinkedNode<E> nodeToInsert){
+		if(firstNode==null){
+			nodeToInsert.previous=null;
+			nodeToInsert.next=null;
+			firstNode=nodeToInsert;
+			lastNode=nodeToInsert;
+		}else{
+			insertBefore(firstNode,nodeToInsert);
+		}
+	}
+	public void insertEnd(DoubleLinkedNode<E> nodeToInsert){
+		if(lastNode==null){
+			insertBeginning(nodeToInsert);
+		}else{
+			insertAfter(lastNode, nodeToInsert);
+		}
+	}
+	private boolean isNodeInTheList (DoubleLinkedNode <E> node){
+		boolean nodeIsInTheList=false;
+		DoubleLinkedNode<E> n=firstNode;
+		
+		while(n!=null && !nodeIsInTheList){
+			if(n==node){
+				nodeIsInTheList=true;
+			}
+			n=n.next;
+		}
+		
+		return nodeIsInTheList;
+	}
+	public void remove (DoubleLinkedNode<E> node){
+		if(!isNodeInTheList(node)){
+			throw new RuntimeException("ERROR: The node is not in the list.");
+		}else{
+			if(node.previous==null){
+				firstNode=node.next;
+			}else{
+				node.previous.next=node.next;
+			}
+			
+			if(node.next==null){
+				lastNode=node.previous;
+			}else{
+				node.next.previous=node.previous;
+			}
+		}
+		
+	}
+	public void forwardsTraverse(){
+		DoubleLinkedNode<E> node=firstNode;
+		
+		System.out.print("Forwards: ");
+		while(node!=null){
+			System.out.print(node.data + " ");
+			node=node.next;
+		}
+		System.out.println("\n");
+	}
+	public void BackwardsTraverse(){
+		DoubleLinkedNode<E> node=lastNode;
+		
+		System.out.print("Backwards: ");
+		while(node!=null){
+			System.out.print(node.data + " ");
+			node=node.previous;
+		}
+		System.out.println("\n");
+	}
 }
